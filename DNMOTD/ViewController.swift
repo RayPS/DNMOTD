2cf92e8adaea049b4cc5cf194c966539fc6484c1
Ray <ray@rayps.com>
2017-06-15 05:40:46 +0800

    add dot button

--- DNMOTD/ViewController.swift
+++ DNMOTD/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var votesLabel: UILabel!
     @IBOutlet weak var loadingEffectView: UIView!
+    @IBOutlet weak var dotButton: UIButton!
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -28,6 +29,7 @@ class ViewController: UIViewController {
         
         messageLabel.layer.opacity = 0
         votesLabel.layer.opacity = 0
+        dotButton.layer.opacity = 0.1
         Loader.addLoadersTo(loadingEffectView)
         
         getMOTD { (json) in
@@ -52,6 +54,7 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.5) { 
             self.messageLabel.layer.opacity = 1
             self.votesLabel.layer.opacity = 1
+            self.dotButton.layer.opacity = 1
         }
     }
 
