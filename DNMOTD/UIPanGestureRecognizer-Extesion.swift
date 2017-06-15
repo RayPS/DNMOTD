3e589e7ec52c44d259bb305fa36e4ba6be8912dd
Ray <ray@rayps.com>
2017-06-16 05:05:17 +0800

    UIPanGestureRecognizer Extesion

---
+++ DNMOTD/UIPanGestureRecognizer-Extesion.swift
@@ -0,0 +1,41 @@
+//
+//  UIPanGestureRecognizer-Extesion.swift
+//  DNMOTD
+//
+//  Created by Ray on 16/06/2017.
+//  Copyright © 2017 rayps. All rights reserved.
+//
+
+import UIKit
+
+// https://stackoverflow.com/questions/35055723/how-to-know-pan-gesture-change-direction-in-ios
+
+extension UIPanGestureRecognizer {
+    
+    public struct PanGestureDirection: OptionSet {
+        public let rawValue: UInt8
+        
+        public init(rawValue: UInt8) {
+            self.rawValue = rawValue
+        }
+        
+        static let Up = PanGestureDirection(rawValue: 1 << 0)
+        static let Down = PanGestureDirection(rawValue: 1 << 1)
+        static let Left = PanGestureDirection(rawValue: 1 << 2)
+        static let Right = PanGestureDirection(rawValue: 1 << 3)
+    }
+    
+    private func getDirectionBy(velocity: CGFloat, greater: PanGestureDirection, lower: PanGestureDirection) -> PanGestureDirection {
+        if velocity == 0 {
+            return []
+        }
+        return velocity > 0 ? greater : lower
+    }
+    
+    public func direction(in view: UIView) -> PanGestureDirection {
+        let velocity = self.velocity(in: view)
+        let yDirection = getDirectionBy(velocity: velocity.y, greater: PanGestureDirection.Down, lower: PanGestureDirection.Up)
+        let xDirection = getDirectionBy(velocity: velocity.x, greater: PanGestureDirection.Right, lower: PanGestureDirection.Left)
+        return xDirection.union(yDirection)
+    }
+}
