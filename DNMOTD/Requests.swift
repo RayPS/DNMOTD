39e1163a77bec8920377bd679fb2e40858530717
RayPS <ray@rayps.com>
2017-08-11 03:11:14 +0800

    set navigateDireaction to default -1

--- DNMOTD/Requests.swift
+++ DNMOTD/Requests.swift
@@ -11,7 +11,7 @@ import Haneke
 
 var currentID: Int = 0
 var latestID: Int = 0
-var navigateDireaction: Int = 0
+var navigateDireaction: Int = -1
 
 let motdsCache = Cache<JSON>(name: "motds")
 let usersCache = Cache<JSON>(name: "users")
