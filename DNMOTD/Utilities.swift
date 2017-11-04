89bcc1928ea113c7fb4db583a34403d85848a144
Ray <ray@rayps.com>
2017-11-05 06:29:06 +0800

    remove TopAlignedLabel

--- DNMOTD/Utilities.swift
+++ DNMOTD/Utilities.swift
@@ -62,28 +62,6 @@ public enum Haptic {
 }
 
 
-@IBDesignable class TopAlignedLabel: UILabel {
-    override func drawText(in rect: CGRect) {
-        if let stringText = text {
-            let stringTextAsNSString = stringText as NSString
-            let labelStringSize = stringTextAsNSString.boundingRect(
-                with: CGSize(width: self.frame.width,height: CGFloat.greatestFiniteMagnitude),
-                options: NSStringDrawingOptions.usesLineFragmentOrigin,
-                attributes: [NSFontAttributeName: font],
-                context: nil).size
-            super.drawText(in: CGRect(x:0,y: 0,width: self.frame.width, height:ceil(labelStringSize.height)))
-        } else {
-            super.drawText(in: rect)
-        }
-    }
-    override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
-        layer.borderWidth = 1
-        layer.borderColor = UIColor.black.cgColor
-    }
-}
-
-
 extension UILabel {
     func preferredHeight(withText: String? = nil) -> CGFloat {
         let text = withText ?? self.text ?? "\n"
